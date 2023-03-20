35f136f8fe3756a1ecdd658efc1cc15c16ca3255
sajuni11 <sajuni11@dotincorp.com>
2023-03-20 12:15:44 +0900

    프로젝트 세팅 초기 커밋

---
+++ src/page/BlogSearch.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>테스트중</div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+	name: "BlogSearch",
+	setup() {},
+});
+</script>
